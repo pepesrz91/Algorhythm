f7ae5384e21c18e00de16691ab540ba545345b4c
Pepe <Krakenn@joses-imac.local>
2016-08-20 12:28:08 -0500

    Initial Commit

---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Jose Juan Suarez on 8/20/16.
+//  Copyright © 2016 Jose Juan Suarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
