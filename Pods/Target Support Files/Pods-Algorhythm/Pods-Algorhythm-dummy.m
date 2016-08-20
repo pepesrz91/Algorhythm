c600bed7f54cf3dc8414473c10b9e2f1a59fcad4
Pepe <Krakenn@joses-imac.local>
2016-08-20 14:03:47 -0500

    Added Firebase

---
+++ Pods/Target Support Files/Pods-Algorhythm/Pods-Algorhythm-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Algorhythm : NSObject
+@end
+@implementation PodsDummy_Pods_Algorhythm
+@end
---
+++ Pods/Target Support Files/Pods-AlgorhythmTests/Pods-AlgorhythmTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AlgorhythmTests : NSObject
+@end
+@implementation PodsDummy_Pods_AlgorhythmTests
+@end
