0e592f465d13b044b5417871dafff3905c7bc697
Pepe <Krakenn@joses-imac.local>
2016-08-21 14:50:50 -0500

    Made Library model.

---
+++ Algorhythm/MusicLibrary.h
@@ -0,0 +1,15 @@
+//
+//  MusicLibrary.h
+//  Algorhythm
+//
+//  Created by Jose Juan Suarez on 8/21/16.
+//  Copyright © 2016 Jose Juan Suarez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MusicLibrary : NSObject
+
+@property (strong,nonatomic) NSArray *library;
+
+@end
